e75cd00470653bdf847de3473c9e1b82b1c371f3
Andrei Bautu <abautu@gmail.com>
2021-02-01 14:42:06 +0200

    Add release info

--- version.php
+++ version.php
@@ -28,3 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2021012700;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016051900;        // Requires this Moodle version
 $plugin->component = 'datafield_jsignature';  // Full name of the plugin (used for diagnostics)
+$plugin->release   = '1.0';
+$plugin->maturity = MATURITY_STABLE;
